7a7319fff07eb90139fa791051a7125a649bc91c
YasiruMethmal123 <yasiru.methmal@outlook.com>
2025-04-09 00:44:05 +0530

    second commit

---
+++ src/main/java/com/example/Entity/User.java
@@ -0,0 +1,22 @@
+package com.example.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String nic;
+    private String email;
+    private String telno;
+}
